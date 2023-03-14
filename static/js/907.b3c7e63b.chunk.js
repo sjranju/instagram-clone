1251c8c6ce4afb42361b2d43424f175d9b3aa766
sjranju <sjranju@gmail.com>
2023-03-14 20:19:51 +0530

    Deploy Instagram clone web  app to GitHub Pages

---
+++ static/js/907.b3c7e63b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinstagram_clone=self.webpackChunkinstagram_clone||[]).push([[907],{6907:function(t,n,a){a.d(n,{uaK:function(){return e}});var c=a(9983);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M8.2 275.4c0-8.6 3.4-17.401 10-24.001 13.2-13.2 34.8-13.2 48 0l451.8 451.8 445.2-445.2c13.2-13.2 34.8-13.2 48 0s13.2 34.8 0 48L542 775.399c-13.2 13.2-34.8 13.2-48 0l-475.8-475.8c-6.8-6.8-10-15.4-10-24.199z"}}]})(t)}}}]);
+//# sourceMappingURL=907.b3c7e63b.chunk.js.map
