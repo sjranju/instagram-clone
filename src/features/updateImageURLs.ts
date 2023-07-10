a6875c4ccac8ae8a859a824f7946432ee32b5315
sjranju <sjranju@gmail.com>
2023-07-10 20:42:34 +0100

    Working on profile page

--- src/features/updateImageURLs.ts
+++ src/features/updateImageURLs.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store/configStore";
-import { updateUserDetails } from "./allUsersSlice";
+import { setCurrentUser, updateUserDetails } from "./allUsersSlice";
 import cloneDeep from "lodash.clonedeep";
+import { updateCurrentUser } from "firebase/auth";
 
 type initialStateType={
     url:string,
@@ -15,6 +16,7 @@ const initialState:initialStateType={
 export const setImageURL=createAsyncThunk('updateImageURL/setImageURL',async({url,user}:initialStateType,{getState,dispatch})=>{
     const state=getState() as RootState
     const stateCopy=cloneDeep(state.allUsers.users)
+    const currentUser=cloneDeep(state.allUsers.currentUser)
     
     if(stateCopy && stateCopy.find(usr=>usr.username==user) && user){
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -24,6 +26,10 @@ export const setImageURL=createAsyncThunk('updateImageURL/setImageURL',async({ur
             console.log('stateCopy',stateCopy)
             dispatch(updateUserDetails(stateCopy))
         }
+    }
+    if(currentUser && currentUser?.imageURL===undefined && user){
+        currentUser.imageURL=url
+        dispatch(setCurrentUser(currentUser))
     }
 })
 
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,19 +6,32 @@ import { useAppSelector } from '../store/use-state-dispatch'
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const Profile = () => {
     const currentUser = useAppSelector(state => state.allUsers.currentUser)
-    const { user } = useParams()
-    console.log('Profile', currentUser)
+    const posts = useAppSelector(state => state.userPosts.postData)
+    const { username } = useParams()
+    console.log('Profile', username)
     return (
-        <div className='flex flex-col'>
-            <div className="flex flex-row">
-                <div className="">
-                    <img src={currentUser?.imageURL} alt="" className='h-24 w-24' />
-                </div>
-                <div className="flex flex-col">
-                    <div className="flex flex-row">
-                        <div className="text-md">{currentUser?.username}</div>
-                        <button type='button' className='bg-white text-black'>Edit Profile</button>
-                        <MdSettings />
+        <div className=" bg-black ">
+            <div className="flex items-center justify-evenly h-screen w-screen">
+                <div className='flex flex-col justify-evenly items-center'>
+                    <div className="flex flex-row justify-evenly items-center space-x-20">
+                        <div className=" ">
+                            <img src={currentUser?.imageURL} alt="" className='h-40 w-40 rounded-full' />
+                        </div>
+                        <div className="flex flex-col justify-evenly space-y-4">
+                            <div className="flex flex-row items-center justify-evenly space-x-4">
+                                <div className="text-xl text-white">{username}</div>
+                                <button type='button' className='bg-white text-black py-2 px-3 rounded-lg text-sm font-semibold'>Edit Profile</button>
+                                <MdSettings color='white' size={30} />
+                            </div>
+                            <div className="text-white flex flex-row space-x-4">
+                                <div className=""><span className='font-semibold mr-1'>1</span> post</div>
+                                <div className=""><span className='font-semibold mr-1'>{currentUser?.followers?.length}</span>followers</div>
+                                <div className=""><span className='font-semibold mr-1'>{currentUser?.following?.length}</span>following</div>
+                            </div>
+                            <div className="text-white text-sm font-semibold">
+                                {currentUser?.fullName}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
