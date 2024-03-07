ed24cc29991a6c41f0000313a7f60404f6878eda
sjranju <sjranju@gmail.com>
2024-03-07 21:56:44 +0000

    Updates

---
+++ static/js/907.84f631f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinstagram_clone=self.webpackChunkinstagram_clone||[]).push([[907],{6907:(a,t,c)=>{c.d(t,{uaK:()=>n});var e=c(9983);function n(a){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M8.2 275.4c0-8.6 3.4-17.401 10-24.001 13.2-13.2 34.8-13.2 48 0l451.8 451.8 445.2-445.2c13.2-13.2 34.8-13.2 48 0s13.2 34.8 0 48L542 775.399c-13.2 13.2-34.8 13.2-48 0l-475.8-475.8c-6.8-6.8-10-15.4-10-24.199z"}}]})(a)}}}]);
+//# sourceMappingURL=907.84f631f6.chunk.js.map
